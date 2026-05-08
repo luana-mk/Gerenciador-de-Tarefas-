abdaa0d6a6bba4bf1315f4107df9c79fceeb54e1
Luana <bjluna911@gmail.com>
2026-05-08 14:30:08 -0300

    projeto

---
+++ Downloads/projetos_/gerenciador_tarefas_php/projeto/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="dashboard.php" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <br></br>
+
+        <label>Senha:</label>
+        <input type="password" name="senha" required>
+        <br></br>
+
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
